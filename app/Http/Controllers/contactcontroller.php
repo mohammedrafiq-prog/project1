b3cd0efa0f5b49a11d1294f879563baa282e4836
mohammedrafiq-prog <69969397+mohammedrafiq-prog@users.noreply.github.com>
2020-08-20 05:22:24 -0700

    Add files via upload

---
+++ app/Http/Controllers/contactcontroller.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\massege;
+class contactcontroller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function test(Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required',
+            'email'=>'required',
+            'message'=>'required'
+        ]);
+
+        $massege= new massege();
+        $massege->name=$request->name;
+        $massege->email=$request->email;
+        $massege->artical=$request->message;
+        $massege->save();
+
+        return redirect('home');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+       {
+           $masseges= massege::all();
+        return view('inc.massege')->with('masseges',$masseges);
+
+       }
+        
+    
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
